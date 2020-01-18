251a326b37141d347d54b79e91a8d72ced9fc954
glof2 <32849073+glof2@users.noreply.github.com>
2020-01-19 00:28:56 +0100

    Corrected small mistake

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import usersettings as user
 download = sid.ImageDownload()
 
 links = download.getimageurl(clientid=user.clientid, clientsecret=user.clientsecret,
-                             useragent=user.useragent, limit=user.limit,subred=user.subreddit
+                             useragent=user.useragent, limit=user.limit,subred=user.subreddit,
                              sortby = user.sortby, saveurls=False)
 
 download.downloadimages(links, filename=user.filename, foldername=user.foldername)
